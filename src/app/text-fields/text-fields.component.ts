c6fd6f13758f4456ec3d9daf7aeeb61f13d23362
Sydney <sydney@zoros-Mac-mini.local>
2018-01-13 20:53:17 -0500

    use ControlContainer to wire up child form component

--- src/app/text-fields/text-fields.component.ts
+++ src/app/text-fields/text-fields.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: '.text-fields',
   templateUrl: './text-fields.component.html',
+
+  // use this to enable using nested forms
+  viewProviders: [ { provide: ControlContainer, useExisting: NgForm } ]
 })
 export class TextFieldsComponent {
-
 }
