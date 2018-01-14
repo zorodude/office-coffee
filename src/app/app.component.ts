9851097a19d021b7f91b906c018147c71a60af77
Sydney <sydney@zoros-Mac-mini.local>
2018-01-13 21:27:32 -0500

    write out submit() to console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Order } from './order';
 import { OrderService } from './order.service';
+import { TextFieldsComponent } from './text-fields/text-fields.component';
 
 @Component({
   selector: '#app-root',
@@ -9,29 +10,40 @@ import { OrderService } from './order.service';
 })
 export class AppComponent {
 
-  // TODO: handle sub-beverage options
-  beverageOptions = 'handle tea options';
+  order : Order = {
+    name: 'test',
+    beverage: 'dark roast',
+    size: 'md',
+    additives: {
+      milk: 2,
+      cream: 2,
+      sugar: 1
+    }
+  }
+
 
   // submitted = false;
-  // onSubmit() { this.submitted = true; }
+  onSubmit({ value, valid }: { value: Order, valid: boolean }) {
+    console.log(value, valid);
+  }
 
   constructor (private order_service: OrderService) {}
 
-  create_new_order(order: Order): void {
-    order = {
-      name: 'test',
-      beverage: 'dark roast',
-      size: 'md',
-      additives: {
-        milk: 2,
-        cream: 2,
-        sugar: 1
-      }
-    }
-
-    this.order_service.create_order(order).then((new_order: Order) => {
-      console.log('subitting ' +new_order);
-    });
-  }
+  // create_new_order(order: Order): void {
+  //   order = {
+  //     name: 'test',
+  //     beverage: 'dark roast',
+  //     size: 'md',
+  //     additives: {
+  //       milk: 2,
+  //       cream: 2,
+  //       sugar: 1
+  //     }
+  //   }
+  //
+  //   this.order_service.create_order(order).then((new_order: Order) => {
+  //     console.log('subitting ' +new_order);
+  //   });
+  // }
 
 }
