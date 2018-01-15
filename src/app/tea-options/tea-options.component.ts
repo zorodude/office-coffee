b6f59e4dbdc2bf454380d85186774c1ed305d3e5
Sydney <sydney@zoros-Mac-mini.local>
2018-01-14 19:21:25 -0500

    wire up tea options to form

--- src/app/tea-options/tea-options.component.ts
+++ src/app/tea-options/tea-options.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: '.tea-options',
   templateUrl: './tea-options.component.html',
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }]
 })
 export class TeaOptionsComponent {
 
   teas = ['Peppermint','Green Tea','Earl Grey','English Breakfast','Orange Pekoe','Honey Lemon','Apple Cinnamon','Chamomile','Chai Tea','Steeped (loose leaf)'];
+  default : String;
+
+  ngOnInit() {
+    this.default = this.teas[0];
+  }
 
 }
