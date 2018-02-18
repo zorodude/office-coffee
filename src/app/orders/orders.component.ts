58d9f83b4b74f8c19d96735ea00bc69144873045
Sydney <sydney@zoros-Mac-mini.local>
2018-02-17 21:21:31 -0500

    simple style up on orders table

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -5,6 +5,7 @@ import { OrderService } from '../order.service';
 @Component({
   selector: '.orders',
   templateUrl: './orders.component.html',
+	styleUrls: ['./orders.css'],
   providers: [ OrderService ]
 })
 
